1732baade0529e09a8255cfc938a9e2bcb16cb59
LaneBrantley <122322578+LaneBrantley@users.noreply.github.com>
2023-06-25 20:22:51 -0400

    feat: moved some code into methods inside of Customer object to promote OO programming. Added comments to code to read easier.

--- Customer.cpp
+++ Customer.cpp
@@ -1,10 +1,14 @@
 #include <string>
+#include <iostream>
+#include <iomanip> // Include the <iomanip> header for formatting
+
 class Customer {
     public:
+        //All attributes of Customer objects
         std::string name, username, password;
         int balance, accNumber;
 
-        Customer() {}
+        Customer() {} //Default constructor
 
         //Constructor with basic variables expected in bank accounts. Note: balance is in cents to avoid floating point errors, ex. 10000 = $100.00
         Customer(std::string name, int balance, int accNumber, std::string username, std::string password) {
@@ -14,5 +18,82 @@ class Customer {
             this->username = username;
             this->password = password;
         }
-    
+
+        //Method for returning a string with the amount in cents to dollar format. ex. if amount = 12345, this returns $123.45
+        std::string centsToDollar(int amount) {
+            int dollars, cents;
+            std::string formattedAmount;
+            dollars = amount / 100;
+            cents = amount % 100;
+            formattedAmount = "$" + std::to_string(dollars) + ".";
+            if (cents < 10) {
+                formattedAmount += "0";
+            }
+            formattedAmount += std::to_string(cents);
+            return formattedAmount;
+        }
+
+        //Method for depositing into bank account. Works in the way of 
+        void deposit() {
+            int amount;
+            std::cout << "How much would you like to deposit? Enter in cents, i.e. 100 = 1.00: ";
+            std::cin >> amount;
+
+            if (amount <= 0) {
+                std::cout << "Amount entered must be greater than 0. Please try again.";
+            }
+            else {
+                balance += amount;
+                std::cout << centsToDollar(amount) << " has been added to your account" << std::endl;
+                std::cin.ignore();
+            }
+        }
+        //Method for withdrawing from account. Logic is: if greater than balance, no withdraw. If not positive, no withdraw. Else, withdraws from account. 
+        void withdraw() {
+            int amount;
+            std::cout << "How much would you like to withdraw? Enter in cents, i.e. 100 = 1.00: ";
+            std::cin >> amount;
+
+            if (amount > balance) {
+                std::cout << "You do not have that much in your account. Your balance is: " << balance;
+            }
+            else if (amount <= 0) {
+                std::cout << "Amount entered must be greater than 0. Please try again. " << std::endl;
+            }
+            else {
+                balance -= amount;
+                std::cout << centsToDollar(amount) << " has been withdrawn from your account" << std::endl;
+                std::cin.ignore();
+            }
+        }
+        // Method to show balance in dollar format instead of in cents
+        void getBalance() {
+            std::cout << "Balance: " << centsToDollar(balance) << std::endl;
+        }
+
+        //Accessor for account number. Requires password verification to access it for privacy reasons
+        void getAccNumber() {
+            std::string input;
+            int passwordFails = 0; //Variable to see if user fails password too many times. If failed 3 times, function will not return account number
+            std::cout << "Enter your password: ";
+            std::cin >> input;
+            while (input != password) {
+                std::cin.ignore();
+                passwordFails++;
+                if (passwordFails >= 3) {
+                    break;
+                }
+                std::cout << "Incorrect. Try again" << std::endl;
+                std::cout << "Enter your password: ";
+                std::cin >> input;
+            }
+
+            if (passwordFails < 3) {
+                std::cout << "Your account number is: " << accNumber << std::endl;
+                std::cin.ignore();
+            }
+            else {
+                std::cout << "Failed password too many times. Try again later." << std::endl;
+            }
+        }
 };
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,80 +3,112 @@
 #include <cctype> 
 #include <algorithm>
 #include <vector>
+#include <string>
 using namespace std;
+#include <random>
 
 // std::transform(input.begin(), input.end(), input.begin(),
 // [](unsigned char c){ return std::tolower(c); }); .tolower wasn't working so had to find another method of changing to lowercase, found this on stackoverflow
 //LOWERCASE FUNCTION
 
 std::vector<Customer> customerArray;  // vector to store customers in order to store their information to be accessed
-Customer currCus;
+Customer currCus; // Customer that is accessed by the program to call during the program
 
 void createAccount() {
     std::string username, password, name;
     std::cout << "Enter your name, first only: ";
     std::cin >> name;
-    std::cout << "Enter your username. Make sure to get it right, as when entered it cannot be changed:";
+    std::cout << "Enter your username. Make sure to get it right, as when entered it cannot be changed: ";
     std::cin >> username;
     std::cout << "Enter your password. Make sure to remember it, as there is no way to access it otherwise: ";
     std::cin >> password;
-    Customer newCustomer(name, 0, 0, username, password);
+
+    //This block is a random number generator to make account number random for new customer
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(1, 10000000);
+    int randomNum = distribution(generator);
+
+    Customer newCustomer(name, 0, randomNum, username, password);
     customerArray.push_back(newCustomer);  // Push the new customer object into the vector
+    std::cin.ignore(); //Clear the std::cin for the next call, otherwise attempLogin will print the prompt twice
 }
 
+//Function that handles logging into account
 bool attemptLogin() {
-    std::string username, password, input;
-    std::cout << "Login: Enter username\nIf you do not have an account, please enter \"Sign up\"\nEnter \"Q\" to quit: ";
-    std::getline(std::cin, input);
-
-    while (input.compare("Q") != 0) {
-        if (input == "Sign up") {
+    std::string input, username, password; // Variables necessary for login
+    while (input.compare("Q") != 0) { //Q is the quit button, so when input is "Q", the loop breaks and ends
+        std::cout << "Login: Enter username\nIf you do not have an account, please enter \"Sign up\"\nEnter \"Q\" to quit: " << std::endl;
+        std::getline(std::cin, input);
+        if (input == "Sign up") { // Calls the createAccount method to create a new customer object 
             createAccount();
-        } else {
+        } 
+        else { // If it is a username, goes through algorithm to find customer
             bool foundUsername = false;
             Customer tempCus;
-            for (int i = 0; i < customerArray.size(); i++) {
+            for (int i = 0; i < customerArray.size(); i++) { //Loops through customer vector
                 if (customerArray[i].username == input) {
                     foundUsername = true;
-                    tempCus = customerArray[i];
+                    tempCus = customerArray[i]; // Copies object to tempCus to allow for password checking
                     break;
                 }
             }
-            if (foundUsername) {
+            if (foundUsername) { //If one of the customers in the vector matches the username entered, password is verified
                 std::cout << "Enter password: ";
                 std::cin >> password;
                 if (password.compare(tempCus.password) == 0) {
                     std::cout << "Login successful!\n";
-                    currCus = tempCus;
-                    return true;
-                } else {
+                    currCus = tempCus; // If password is correct, currCus copies the object's information to allow modification
+                    return true; // Returns true to allow mainProgram() to go into the if block 
+                } 
+                else {
                     std::cout << "Incorrect password. Try again.\n";
+                    std::cin.ignore();
                 }
-            } else {
+            } 
+            else if (input == "Q") {
+                return false; //Returns false if q is entered, causing the program to end. 
+            }
+            else {
                 std::cout << "Username not found. Try again or sign up.\n";
             }
         }
-        std::cout << "Login: Enter username\nIf you do not have an account, please enter \"Sign up\"\nEnter \"Q\" to quit: ";
-        std::cin >> input;
     }
-
-    return false;
 }
 
+//Control of the program. If the user logs in, it goes into the if block to allow the user to use the bank account. 
 void mainProgram() {
-    bool login = attemptLogin();
-    if (login) {
-        std::cout << "Name: " + currCus.name;
-        std::cout << "Balance: " + currCus.balance;
-    }
-    else {
+    std::string input;
+    if (attemptLogin()) { //If attemptLogin() returns true: 
 
+        while (input != "q") { //If q is entered, the app shuts down
+
+            //Input to determine what the user wants to do
+            std::cout << "Name: " << currCus.name << std::endl;
+            currCus.getBalance();
+            std::cout << "What would you like to do? Enter \"d\" for deposit, \"w\" for withdraw, \"n\" for account number, \"q\" to quit: ";
+            std::getline(std::cin, input);
+
+            //Calls withdraw method on the customer object
+            if (input == "w") {
+                currCus.withdraw();
+            }
+
+            //Calls deposit on the customer object
+            else if (input == "d") {
+                currCus.deposit();
+            }
+
+            //Calls getAccountNumber on customer object
+            else if (input == "n") {
+                currCus.getAccNumber();
+            }
+        }
+        std::cout << "Thank you for using our bank! Goodbye. ";
     }
 }
 
-
-
-
+//Function that is compiled, which calls mainProgram()
 int main() {
     mainProgram();
     
